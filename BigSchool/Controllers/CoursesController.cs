956ddb4bdc716df3cdcb97a615b1fbae36a8d716
Admin <Admin@DESKTOP-00H7G97>
2023-03-05 15:39:01 +0700

    Add project files.

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
